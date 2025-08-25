9dd1ad8ec0c49fe3fe665e78b3c728d55e8f1bb0
aza-coder <59904532+aza-coder@users.noreply.github.com>
2025-08-25 19:14:12 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker для Papéllo PWA
-// Версия: v7.1
+// Версия: v7.2
 
-const SW_VERSION = 'v7.1';
+const SW_VERSION = 'v7.2';
 
 // Имена кэшей с версионированием
 const STATIC_CACHE = 'static-' + SW_VERSION;
